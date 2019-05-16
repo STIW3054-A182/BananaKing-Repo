0cae3a7df1c37d76b4c05a2498a267f5213c9803
Chan Jun Liang <37323892+ChanJunLiang@users.noreply.github.com>
2019-05-17 03:04:45 +0800

    Add files via upload

---
+++ rtproject/src/main/java/com/rtproject/Part7.java
@@ -0,0 +1,72 @@
+package com.rtproject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class Part7 {
+
+	public static void main(String[]args) throws Exception{
+
+		int coreCount = Runtime.getRuntime().availableProcessors();
+		ExecutorService service = Executors.newFixedThreadPool(coreCount);
+        
+        PropertiesFile prop = new PropertiesFile();
+        prop.createProperties();
+		prop.readProperties();
+		Properties pro = new Properties();
+		InputStream input = new FileInputStream("config.properties");
+        pro.load(input);
+        List<String> validLink = new ArrayList<String>();
+        
+		
+
+        Path path = Paths.get(prop.getTextFile());
+
+        List<String> url = Files.readAllLines(path);
+        for (int i = 0; i < url.size(); i++) {
+            ValidLink t = new ValidLink(url.get(i));
+            Future<String> future = service.submit(t);
+                if (future.get()!=null){
+					String valid = future.get();
+					
+					validLink.add(valid);
+                }
+		}
+
+		service.shutdown();
+        try {
+            if (!service.awaitTermination(60, TimeUnit.SECONDS)) {
+                service.shutdownNow();
+
+                if (!service.awaitTermination(60, TimeUnit.SECONDS)) {
+                    //System.err.println("");
+                }
+            }
+
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+
+        for(int x=0; x<validLink.size();x++){
+			DisplayRosli dr = new DisplayRosli(validLink.get(x));
+			dr.saveTable();
+			dr.getTitle();
+			dr.display();
+        }
+	}
+
+}
---
+++ rtproject/src/main/java/com/rtproject/PlayerList.java
@@ -0,0 +1,109 @@
+package com.rtproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerList{
+
+    private List<String> ranklist= new ArrayList<String>();
+    private List<String> snolist= new ArrayList<String>();
+    private List<String> namelist= new ArrayList<String>();
+    private List<String> fedlist= new ArrayList<String>();
+    private List<String> rtglist= new ArrayList<String>();
+    private List<String> club_citylist= new ArrayList<String>();
+    private List<String> ptslist= new ArrayList<String>();
+    private List<String> tb1list= new ArrayList<String>();
+    private List<String> tb2list= new ArrayList<String>();
+    private List<String> tb3list= new ArrayList<String>();
+
+    
+
+
+    public PlayerList(){
+
+    }
+
+    public void setRank(String rank){
+        ranklist.add(rank);
+    }
+
+    public void setSno(String sno){
+        snolist.add(sno);
+    }
+
+    public void setName(String name){
+        namelist.add(name);
+    }
+
+    public void setFed(String fed){
+        fedlist.add(fed);
+    }
+
+    public void setRtg(String rtg){
+        rtglist.add(rtg);
+    }
+    
+    public void setClub_city(String club_city){
+        club_citylist.add(club_city);
+    }
+
+    public void setPts(String pts){
+        ptslist.add(pts);
+    }
+
+    public void setTb1(String tb1){
+        tb1list.add(tb1);
+    }
+
+    public void setTb2(String tb2){
+        tb2list.add(tb2);
+    }
+
+    public void setTb3(String tb3){
+        tb3list.add(tb3);
+    }
+
+    public List<String> getRank(){
+        return ranklist;
+    }
+
+    public List<String> getSno(){
+        return snolist;
+    }
+
+    public List<String> getName(){
+        return namelist;
+    }
+
+    public List<String> getFed(){
+        return fedlist;
+    }
+
+    public List<String> getRtg(){
+        return rtglist;
+    }
+
+    public List<String> getClub_city(){
+        return club_citylist;
+    }
+
+    public List<String> getPts(){
+        return ptslist;
+    }
+
+    public List<String> getTb1(){
+        return tb1list;
+    }
+
+    public List<String> getTb2(){
+        return tb2list;
+    }
+
+    public List<String> getTb3(){
+        return tb3list;
+    }
+
+	public static int size() {
+		return 0;
+	}
+}
